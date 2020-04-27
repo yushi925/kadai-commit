bb509fccc5d90b5dc9e378b3976a4fb516181e23
yushi.kasai <abcd33385@gmail.com>
2020-04-27 13:29:02 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：なし</p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく</button>
+        </form>
+    </body>
+</html>
